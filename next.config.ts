31acc584044c051f2f30c07c3bd4cbcd15f1f5e0
mazztah <154804460+mazztah@users.noreply.github.com>
2026-07-14 16:00:33 +0200

    Add files via upload

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,27 @@
 import type { NextConfig } from "next";
+import path from "path";
 
 const nextConfig: NextConfig = {
   reactStrictMode: true,
   output: "standalone",
   eslint: {
     ignoreDuringBuilds: true,
+  },
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  images: {
+    unoptimized: true,
+  },
+  webpack: (config) => {
+    config.resolve.symlinks = false;
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      react: path.resolve(process.cwd(), "node_modules/react"),
+      "react-dom": path.resolve(process.cwd(), "node_modules/react-dom"),
+      next: path.resolve(process.cwd(), "node_modules/next"),
+    };
+    return config;
   },
 };
 
